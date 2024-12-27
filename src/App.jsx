a1c66c6acd88868faff79aedffb0cefcd21cfc8f
Sarvesh Pandey <158469908+yatrilive@users.noreply.github.com>
2024-12-28 03:20:13 +0530

    Update App.jsx
    contact to Contact

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './components/Home';
 import Experienc from './components/Experience';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
-import Contact from './components/contact';
+import Contact from './components/Contact';
 import Aos from "aos";
 import "aos/dist/aos.css"
 
